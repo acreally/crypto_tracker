2f53f4488a73a8f398147cb8496d0f9fb5353a0b
Andrew Creally <creallya@gmail.com>
2019-04-08 20:09:18 -0400

    Splitting up Binance buy transactions to be converted

--- tracker/strategy/binance.py
+++ tracker/strategy/binance.py
@@ -1,10 +1,26 @@
 import csv
+import re
 
 from strategy.base import BaseStrategy
 from converter.binance import BinanceConverter
 
 
+BASE_MARKET_CURRENCIES = ['BTC', 'ETH', 'BNB']
+
+
 class BinanceStrategy(BaseStrategy):
+  DATE = 'Date(UTC)'
+  MARKET = 'Market'
+  TYPE = 'Type'
+  PRICE = 'Price'
+  AMOUNT = 'Amount'
+  FEE = 'Fee'
+  FEE_COIN = 'Fee Coin'
+
+  CURRENCY = 'Currency'
+
+  TRANSACTION_TYPE_BUY = 'BUY'
+  TRANSACTION_TYPE_SELL = 'SELL'
 
   def __init__(self):
     self.converter = BinanceConverter()
@@ -18,3 +34,47 @@ class BinanceStrategy(BaseStrategy):
         converted_data.append(self.converter.convert(row))
 
     return converted_data
+
+  def _split_transaction(self, transaction):
+    transactions = []
+    market = transaction.get(self.MARKET)
+
+    pattern = '(.*)({})'.format('|'.join(BASE_MARKET_CURRENCIES))
+    currencies = re.findall(pattern, market)
+
+    transaciton_type = transaction.get(self.TYPE)
+
+    if transaciton_type == self.TRANSACTION_TYPE_BUY:
+      transactions += self._split_buy_transaction(transaction, currencies[0][0], transaction.get(self.AMOUNT))
+
+    return transactions
+
+  def _split_buy_transaction(self, transaction, currency, amount):
+    date = transaction.get(self.DATE)
+    fee = transaction.get(self.FEE)
+    fee_currency = transaction.get(self.FEE_COIN)
+    transactions = []
+
+    buy_transaction = {self.DATE: date,
+                       self.CURRENCY: currency,
+                       self.TYPE: self.TRANSACTION_TYPE_BUY,
+                       self.AMOUNT: amount
+                       }
+
+    if transaction.get(self.FEE_COIN) == currency:
+      buy_transaction[self.FEE] = fee
+    else:
+      buy_transaction[self.FEE] = '0'
+      transactions.append(self._build_fee_transaction(date, fee, fee_currency))
+
+    transactions.append(buy_transaction)
+
+    return transactions
+
+  def _build_fee_transaction(self, date, fee, fee_currency):
+    return {self.DATE: date,
+            self.CURRENCY: fee_currency,
+            self.TYPE: self.TRANSACTION_TYPE_BUY,
+            self.AMOUNT: '0',
+            self.FEE: fee
+            }
--- tracker/tests/test_binance_strategy.py
+++ tracker/tests/test_binance_strategy.py
@@ -29,3 +29,60 @@ class BinanceStrategyTest(unittest.TestCase):
     result = self.strategy.convert_data(helpers.get_absolute_file_path(filename, *file_path))
 
     self.assertEqual(expected, result)
+
+  def test_split_transaction_buy_with_same_fee_currency_returns_only_buy_transaction(self):
+    # Date(UTC),Market,Type,Price,Amount,Total,Fee,Fee Coin
+    # 2018-01-20 16:59:59,XLMETH,BUY,0.00012345,123,0.01518435,0.123,XLM
+    expected_buy = {'Date(UTC)': '2018-01-20 16:59:59',
+                    'Currency': 'XLM',
+                    'Type': 'BUY',
+                    'Amount': '123',
+                    'Fee': '0.123'
+                    }
+    expected = [expected_buy]
+
+    transaction = {'Date(UTC)': '2018-01-20 16:59:59',
+                   'Market': 'XLMETH',
+                   'Type': 'BUY',
+                   'Price': '0.00012345',
+                   'Amount': '123',
+                   'Total': '0.01518435',
+                   'Fee': '0.123',
+                   'Fee Coin': 'XLM'
+                   }
+
+    self._assert_split_transaction(expected, transaction)
+
+  def test_split_transaction_buy_with_different_fee_currency_returns_buy_and_fee_transactions(self):
+    # Date(UTC),Market,Type,Price,Amount,Total,Fee,Fee Coin
+    # 2018-05-11 21:42:11,ZILBTC,BUY,0.00001111,999,0.01109889,0.00077777,BNB
+    expected_fee = {'Date(UTC)': '2018-05-11 21:42:11',
+                    'Currency': 'BNB',
+                    'Type': 'BUY',
+                    'Amount': '0',
+                    'Fee': '0.00077777'
+                    }
+    expected_buy = {'Date(UTC)': '2018-05-11 21:42:11',
+                    'Currency': 'ZIL',
+                    'Type': 'BUY',
+                    'Amount': '999',
+                    'Fee': '0'
+                    }
+    expected = [expected_fee, expected_buy]
+
+    transaction = {'Date(UTC)': '2018-05-11 21:42:11',
+                   'Market': 'ZILBTC',
+                   'Type': 'BUY',
+                   'Price': '0.00001111',
+                   'Amount': '999',
+                   'Total': '0.01518435',
+                   'Fee': '0.00077777',
+                   'Fee Coin': 'BNB'
+                   }
+
+    self._assert_split_transaction(expected, transaction)
+
+  def _assert_split_transaction(self, expected, transaction):
+    result = self.strategy._split_transaction(transaction)
+
+    self.assertEqual(expected, result)
